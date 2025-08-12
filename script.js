aa9962869e85753954f421ebc33a8e4423178e46
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-08-12 15:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
+	const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let result = "";
+	let n = chars.length;
+
+	for (let i = 0; i < l; i++){
+		result += chars.charAt(Math.floor(Math.random() * n));
+	}
+
+	return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+
